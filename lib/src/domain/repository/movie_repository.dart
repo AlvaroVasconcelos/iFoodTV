c21f99334232f0cdcb9232c64421ab5f70db324f
Vasconcelos.dev <alvaro@fteam.dev>
2024-07-10 11:59:44 -0300

    fix: rework core fixed

--- lib/src/domain/repository/movie_repository.dart
+++ lib/src/domain/repository/movie_repository.dart
@@ -1,7 +1,7 @@
-import '../../entity/movie.dart';
-import '../error.dart';
-import '../result.dart';
-import 'repository.dart';
+import '../entity/movie.dart';
+import '../core/error.dart';
+import '../core/result.dart';
+import '../core/repository.dart';
 
 abstract class MovieRepository extends Repository {
   Future<Result<Movie, BaseError>> getMovie(String id);
